947965e44c28c3e33f127a9dee9cf94af379ac2d
twinchester <tannerwinchester2@gmail.com>
2021-11-26 11:23:59 -0700

    Add edit delete to artist and disk.

--- DiskInventory/Controllers/ArtistController.cs
+++ DiskInventory/Controllers/ArtistController.cs
@@ -19,5 +19,56 @@ namespace DiskInventory.Controllers
             List<Artist> artists = context.Artists.OrderBy(a => a.ArtistName).ToList();
             return View(artists);
         }
+        [HttpGet]
+        public IActionResult Add()
+        {
+            ViewBag.Action = "Add";
+            ViewBag.ArtistTypes = context.ArtistTypes.OrderBy(t => t.Description).ToList();
+            return View("Edit", new Artist());
+        }
+        [HttpGet]
+        public IActionResult Edit(int id)
+        {
+            ViewBag.Action = "Edit";
+            ViewBag.ArtistTypes = context.ArtistTypes.OrderBy(t => t.Description).ToList();
+            var artist = context.Artists.Find(id);
+            return View(artist);
+        }
+        [HttpPost]
+        public IActionResult Edit(Artist artist)
+        {
+            if (ModelState.IsValid)
+            {
+                if (artist.ArtistId == 0)   // means add the artist
+                {
+                    context.Artists.Add(artist);
+                }
+                else                       // means update the artist
+                {
+                    context.Artists.Update(artist);
+                }
+                context.SaveChanges();
+                return RedirectToAction("Index", "Artist");
+            }
+            else
+            {
+                ViewBag.Action = (artist.ArtistId == 0) ? "Add" : "Edit";
+                ViewBag.ArtistTypes = context.ArtistTypes.OrderBy(t => t.Description).ToList();
+                return View(artist);
+            }
+        }
+        [HttpGet]
+        public IActionResult Delete(int id)
+        {
+            var artist = context.Artists.Find(id);
+            return View(artist);
+        }
+        [HttpPost]
+        public IActionResult Delete(Artist artist)
+        {
+            context.Artists.Remove(artist);
+            context.SaveChanges();
+            return RedirectToAction("index", "Artist");
+        }
     }
 }
--- DiskInventory/Controllers/DiskController.cs
+++ DiskInventory/Controllers/DiskController.cs
@@ -19,5 +19,68 @@ namespace DiskInventory.Controllers
             List<Disk> disk = context.Disks.OrderBy(d => d.CdId).ThenBy(d => d.CdName).ThenBy(d => d.ReleaseDate).ThenBy(d => d.ArtistId).ThenBy(d => d.GenreId).ThenBy(d => d.StatusId).ThenBy(d => d.DiskTypeId).ToList();
             return View(disk);
         }
+        [HttpGet]
+        public IActionResult Edit(int id)
+        {
+            ViewBag.Action = "Edit";
+            ViewBag.DiskTypes = context.DiskTypes.OrderBy(t => t.Description).ToList();
+            ViewBag.Statuses = context.Statuses.OrderBy(s => s.Description).ToList();
+            ViewBag.GenreTypes = context.GenreTypes.OrderBy(g => g.Description).ToList();
+            ViewBag.ArtistIds = context.Artists.OrderBy(d => d.ArtistId).ToList();
+            var disk = context.Disks.Find(id);
+            return View(disk);
+        }
+        [HttpPost]
+        public IActionResult Edit(Disk disk)
+        {
+            if (ModelState.IsValid)
+            {
+                if (disk.CdId == 0)
+                {
+                    context.Disks.Add(disk);
+                }
+                else
+                {
+                    context.Disks.Update(disk); 
+                }
+                context.SaveChanges();
+                return RedirectToAction("Index", "Disk");
+            }
+            else
+            {
+                ViewBag.Action = (disk.CdId == 0) ? "Add" : "Edit";
+                ViewBag.DiskTypes = context.DiskTypes.OrderBy(t => t.Description).ToList();
+                ViewBag.GenreTypes = context.GenreTypes.OrderBy(g => g.Description).ToList();
+                ViewBag.Statuses = context.Statuses.OrderBy(s => s.Description).ToList();
+                ViewBag.ArtistIds = context.Artists.OrderBy(d => d.ArtistId).ToList();
+                return View(disk);
+            }
+        }
+        [HttpGet]
+        public IActionResult Delete(int id)
+        {
+            var disk = context.Disks.Find(id);
+            return View(disk);
+        }
+        [HttpPost]
+        public IActionResult Delete(Disk disk)
+        {
+            context.Disks.Remove(disk);
+            context.SaveChanges();
+            return RedirectToAction("Index", "Disk");
+        }
+        [HttpGet]
+        public IActionResult Add()
+        {
+            ViewBag.Action = "Add";
+            ViewBag.DiskTypes = context.DiskTypes.OrderBy(t => t.Description).ToList();
+            ViewBag.Statuses = context.Statuses.OrderBy(s => s.Description).ToList();
+            ViewBag.GenreTypes = context.GenreTypes.OrderBy(g => g.Description).ToList();
+            ViewBag.ArtistIds = context.Artists.OrderBy(d => d.ArtistId).ToList();
+            Disk disk = new Disk();
+            return View("Edit", disk);
+        }
+
     }
+
 }
